aa7429c801c755c87d37a792577f9c5f6ca8c010
Antonellaotegui <aotegui@udesa.edu.ar>
2023-11-06 15:58:51 -0300

    firebase

---
+++ scr/components/firebase/config.js
@@ -0,0 +1,17 @@
+import app from "firebase/app";
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDGofqA0bAAyHvrDm3gXd4gPERMjNoS1w4",
+    authDomain: "segunda-parte-c34ad.firebaseapp.com",
+    projectId: "segunda-parte-c34ad",
+    storageBucket: "segunda-parte-c34ad.appspot.com",
+    messagingSenderId: "68686650264",
+    appId: "1:68686650264:web:6081bc60e2c229aa6f69ed"
+  };
+  
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const storage = app.storage();
+export const db = app.firestore();
